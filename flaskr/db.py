a0b0d927f85e1b4843bb31b4fe6371063b9521aa
muriukisam <mr.muriukisammy@gmail.com>
2022-02-12 01:51:51 +0300

    Created auth blueprint

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,8 +6,8 @@ from flask.cli import with_appcontext
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['database'],
-            detect_types=sqlite3.PARSE_DECTYPES
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row #tells the connection to return rows that behave like dicts. This allows accessing the columns by name.
     return g.db
